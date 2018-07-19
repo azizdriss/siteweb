5eb1362998f5fc17f4301142468c3fd65ea68e00
aziz <aziz.driss531@gmail.com>
2018-07-19 21:22:41 +0100

    siteweb

---
+++ projetfinal/jav.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $(".slide2").hover(function(){
+        $(".slide2").css("transform","translate(200px,0)");
+        $(".slide2").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide3").hover(function(){
+        $(".slide3").css("transform","translate(200px,0)");
+        $(".slide3").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide4").hover(function(){
+        $(".slide4").css("transform","translate(200px,0)");
+        $(".slide4").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide5").hover(function(){
+        $(".slide5").css("transform","translate(200px,0)");
+        $(".slide5").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide6").hover(function(){
+        $(".slide6").css("transform","translate(200px,0)");
+        $(".slide6").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide7").hover(function(){
+        $(".slide7").css("transform","translate(200px,0)");
+        $(".slide7").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide8").hover(function(){
+        $(".slide8").css("transform","translate(200px,0)");
+        $(".slide8").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide9").hover(function(){
+        $(".slide9").css("transform","translate(200px,0)");
+        $(".slide9").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide10").hover(function(){
+        $(".slide10").css("transform","translate(200px,0)");
+        $(".slide10").css("opacity", 1);
+
+    });
+});
+$(document).ready(function(){
+    $(".slide11").hover(function(){
+        $(".slide11").css("transform","translate(200px,0)");
+        $(".slide11").css("opacity", 1);
+
+    });
+});
